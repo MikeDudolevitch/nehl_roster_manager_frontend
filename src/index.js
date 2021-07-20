317dd4e487b40dc3215b291d750d8e08fe595f9f
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-07-20 17:42:16 -0400

    begin writing func/methods to render team to dom

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoadec', () => {
-    
+    getTeam()
 })
+
+function getTeam(){
+    fetch('http://localhost:3000/api/teams')
+    .then(r => r.json())
+    .then(resp => {
+        resp.forEach(team => {
+            const t = new Team(team)
+            t.addToDom()
+        })
+    })
+}
--- src/team.js
+++ src/team.js
@@ -1,12 +1,14 @@
 class Team {
-    constructor(name, logo_img) {
+    constructor({name, logo_img}) {
         this.name = name
         this.logo_img = logo_img
         Team.all.push(this)
     }
     static all = []
 
-
+    addToDom() {
+        const teamOnDom = document.querySelector('#team-container')
+    }
 
 
 }

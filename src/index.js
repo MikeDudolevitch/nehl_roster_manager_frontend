e25fdff8aa7e17d30a159e07b0c5ba6e55d3f37f
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-07-21 15:15:44 -0400

    render modal

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
     getTeam()
 })
 
+closeModalBtn.addEventListener('click', () => {
+    modal.classList.add("hide")
+})
+
+
+
 function getTeam(){
     fetch('http://localhost:3000/api/teams')
     .then(r => r.json())
     .then(resp => {
         resp.forEach(team => {
-            const t = new Team(team)
+            const t = new Team(team, modal)
             t.addToDom()
         })
     })
--- src/team.js
+++ src/team.js
@@ -1,18 +1,25 @@
 class Team {
-    constructor({name, logo_img}) {
+    constructor({name, logo_img}, modal) {
         this.name = name
         this.logo_img = logo_img
+        this.modal = modal
         Team.all.push(this)
     }
     static all = []
 
     addToDom() {
         const teamOnDom = document.querySelector('#team-container')
-        teamOnDom.innerHTML += this.renderTeam()
+        const teamDiv = document.createElement("div")
+        teamDiv.classList.add("team")
+        teamDiv.innerHTML += this.renderTeam()
+        teamOnDom.appendChild(teamDiv)
+        teamDiv.addEventListener('click', () => {
+            this.modal.classList.remove("hide")
+        })
     }
 
     renderTeam() {
-        return (`<div class= "team"><h3> ${this.name} </h3>
-        <img class= "team-image" src= "${this.logo_img}" /></div>`) 
+        return (`<h3> ${this.name} </h3>
+        <img class= "team-image" src= "${this.logo_img}" />`) 
     }
 }

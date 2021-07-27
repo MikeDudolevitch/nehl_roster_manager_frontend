331e94ee2a60e0633fb6d8b724fccfa4b0419bb6
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-07-27 11:57:26 -0400

    start refactor addtodom method

--- src/team.js
+++ src/team.js
@@ -8,29 +8,32 @@ class Team {
     }
     static all = []
 
-    
+    const modalContent = this.modal.querySelector("#modal-content")
+    const playerContent = document.createElement("div")
+
     addToDom() {
         const teamOnDom = document.querySelector('#team-container')
         const teamDiv = document.createElement("div")
         teamDiv.classList.add("team")
         teamDiv.innerHTML += this.renderTeam()
         teamOnDom.appendChild(teamDiv)
-        const modalContent = this.modal.querySelector("#modal-content")
+        
         teamDiv.addEventListener('click', () => {
             this.modal.classList.remove("hide")
-            fetch(`http://localhost:3000/api/teams/${this.id}`)
+    }
+
+    static getPlayers () {
+        fetch(`http://localhost:3000/api/teams/${this.id}`)
             .then(r => r.json())
             .then( (t) => {
                 t.players.forEach(player => {
-                    const playerContent = document.createElement("div")
                     playerContent.classList.add("player")
-                    playerContent.innerHTML += `Name: ${player.first_name} ${player.last_name} Shoots: ${player.handedness} Position: ${player.primary_position}`
-                    modalContent.appendChild(playerContent)
-                })
-                    
+                    renderPlayer(player)}
+                modalContent.append(playerContent)
+    }
 
-            })
-        })
+    renderPlayer() {
+        playerContent.innerHTML = `Name: ${this.players.first_name} ${this.players.last_name}`
     }
 
     renderTeam() {
